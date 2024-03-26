95665210513adfa07a3db627f5a9fe64da5f0ff9
Ines Mzyene <emmawasnotthere@gmail.com>
2024-03-26 17:11:04 +0100

    third

--- Kotlin/src/Array.kt
+++ Kotlin/src/Array.kt
@@ -1,25 +1,2 @@
-val planetes = arrayOf("mercure", "Venus", "terre", "Mars", "Jupiter", "saturne",
-        "Uranus", "Neptune")
-
-
-fun elements () {
-    println(planetes.first())
-    println(planetes.last())
-
-    println(planetes[0])
-    println(planetes[7])
+class Array {
 }
-
-fun indexTerre () {
-    val item = "terre"
-    val index = planetes.indexOf(item)
-    println(index)
-}
-
-fun filter () {
-
-    val filterPlanete = (planetes.filter { it.uppercase().startsWith(char = 'm')})
-    println(filterPlanete)
-}
-
-
